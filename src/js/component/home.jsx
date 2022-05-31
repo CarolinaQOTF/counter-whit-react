cbe82a488aa8701fb5145314602c59cf195c1e36
CarolinaQOTF <carolinacelis012@gmail.com>
2022-05-31 08:34:13 +0000

    subida de proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import reactDom from "react-dom";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Container row d-flex bg-dark p-5 text-white fs-1 text-center">
+			<div className="IconClock col m-2 p-4 border border-white">
+				<i className="far fa-clock "></i>
+			</div>
+			<div className="Number4 col m-2 p-4 border border-white"></div>
+			<div className="Number3 col m-2 p-4 border border-white"></div>
+			<div className="Number2 col m-2 p-4 border border-white"></div>
+			<div className="Number1 col m-2 p-4 border border-white"></div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 //import react into the bundle
+import propTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -8,5 +9,100 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const Seconds = (props) => {
+	return (
+		<div
+			id="ContenedorGeneral"
+			className="Container row d-flex bg-dark p-5 text-white fs-1 text-center">
+			<div className="mb-5">
+				<h1>
+					Simple counter whit React
+					<i className="fab fa-react"></i>
+					<i className="far fa-react "></i>
+				</h1>
+			</div>
+			<div className="IconClock col m-2 p-4 border border-white rounded-3 mb-5">
+				<i className="far fa-clock fa-lg"></i>
+			</div>
+			<div className="Number4 col m-2 p-4 border border-white rounded-3 mb-5">
+				{props.digitFour}
+			</div>
+			<div className="Number3 col m-2 p-4 border border-white rounded-3 mb-5">
+				{props.digitThree}
+			</div>
+			<div className="Number2 col m-2 p-4 border border-white rounded-3 mb-5">
+				{props.digitTwo}
+			</div>
+			<div className="Number1 col m-2 p-4 border border-white rounded-3 mb-5">
+				{props.digitOne}
+			</div>
+			<div>
+				<button
+					type="button"
+					className="btn btn-primary btn-lg"
+					onClick={() => {
+						clearInterval(intervalos);
+					}}>
+					Stop
+				</button>
+				<button
+					type="button"
+					className="btn btn-primary btn-lg"
+					onClick={() => pause()}>
+					Start
+				</button>
+				<button
+					type="button"
+					className="btn btn-primary btn-lg"
+					onClick={() => reset()}>
+					Reset
+				</button>
+			</div>
+		</div>
+	);
+};
+
+// Home.propTypes = {
+// 	digit1: propTypes.number,
+// 	digit2: propTypes.number,
+// 	digit3: propTypes.number,
+// 	digit4: propTypes.number,
+// };
+let counter = 0;
+
+function contar() {
+	let four = Math.floor(counter / 1000);
+	let Three = Math.floor(counter / 100);
+	let Two = Math.floor(counter / 10);
+	let one = Math.floor(counter / 1);
+
+	counter++;
+
+	ReactDOM.render(
+		<Seconds
+			digitOne={one}
+			digitTwo={Two}
+			digitThree={Three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}
+contar();
+
+let intervalos = setInterval(contar, 1000);
+
+let onOff = false;
+
+function pause() {
+	if (!onOff) {
+		onOff = false;
+		intervalos = setInterval(contar, 1000);
+	}
+}
+
+function reset() {
+	clearInterval(counter);
+	counter = 0;
+	intervalos = setInterval(contar, 1000);
+}
